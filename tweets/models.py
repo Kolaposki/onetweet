1e6ecc5b31f2bf4e7e54f8a62c8793db246031cc
Oshodi Kolapo <oshodikolapo@gmail.com>
2020-04-22 10:20:36 +0100

    Add all require Django files with todo

---
+++ tweets/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+# Create your models here.
+class Tweet(models.Model):
+    text = models.CharField(blank=True, null=True, max_length=256)
+    image = models.ImageField(blank=True, null=True, upload_to='/images')
