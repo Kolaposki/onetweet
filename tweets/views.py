b63aee6b5e31f2b7517c3f1f0806f2b45ec85279
Oshodi Kolapo <oshodikolapo@gmail.com>
2020-04-22 11:06:14 +0100

    Test response to html views

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Tweet
+from django.http import HttpResponse, Http404, HttpRequest, JsonResponse
+
 
 # Create your views here.
+
+def tweet_detail_view(request):
+    return HttpResponse("<h2>Hello World</h2>")
